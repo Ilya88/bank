2388aa13bfc42c3e7eb21ace465a617cfe661137
ilya_r <magoz@mail.ru>
2015-11-30 02:48:10 +0500

    индекс на уникальность login в миграции

--- migrations/20151129173235-add-index.js
+++ migrations/20151129173235-add-index.js
@@ -5,9 +5,16 @@ module.exports = {
     queryInterface.addIndex(
         'Bills', ['user_id']
     );
+    queryInterface.addIndex(
+        'Users', ['login'], {
+          indexName: 'login',
+          indicesType: 'UNIQUE'
+        }
+    );
   },
 
   down: function (queryInterface, Sequelize) {
     queryInterface.removeIndex('Bills', 'user_id');
+    queryInterface.removeIndex('Users', 'login');
   }
 };
