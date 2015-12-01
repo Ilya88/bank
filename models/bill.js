17e21d29e05ec561b1d2d4fd62818b454ccc07eb
ilya_r <magoz@mail.ru>
2015-12-02 04:57:13 +0500

    проверка на минимальное значение в модели счета

--- models/bill.js
+++ models/bill.js
@@ -3,7 +3,10 @@ module.exports = function(sequelize, DataTypes) {
   var Bill = sequelize.define('Bill', {
     userId: DataTypes.INTEGER,
     currencyId: DataTypes.INTEGER,
-    balance: DataTypes.NUMERIC
+    balance: {
+      type: DataTypes.NUMERIC,
+      validate: { min: 0 }
+    }
   }, {
     classMethods: {
       associate: function(models) {
