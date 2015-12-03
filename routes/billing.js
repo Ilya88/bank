0a77be1bdbcfa7c92eec1001d887ef4c6d96f835
ilya_r <magoz@mail.ru>
2015-12-03 05:11:34 +0500

    функционал для пополнения счетов и для переводов (пока без транзакций)

--- routes/billing.js
+++ routes/billing.js
@@ -40,7 +40,8 @@ router.get('/:id', isAuthenticated, function(req, res) {
     }).then(function(bill) {
         res.render('billing/bill', {
             user: req.user,
-            bill: bill
+            bill: bill,
+            message: req.flash('message')
         });
     });
 });
@@ -49,11 +50,117 @@ router.post('/:id', isAuthenticated, function(req, res) {
     models.Bill.findOne({
         where: {id: req.params.id, userId: req.user.id}
     }).then(function(bill) {
+        var newValue = bill.balance;
+        if (!isNaN(req.body.credit) && req.body.credit > 0) {
+            newValue += Number(req.body.credit);
+        } else if (!isNaN(req.body.debit) && req.body.debit > 0) {
+            newValue -= Number(req.body.debit);
+        } else {
+            req.flash('message', 'Invalid value');
+            return res.redirect('/billing/' + req.params.id);
+        }
         models.Bill.update(
-            { balance: bill.balance - req.body.debit }, {where: {id: req.params.id, userId: req.user.id}}
+            { balance: newValue }, {where: {id: req.params.id, userId: req.user.id}}
         ).then(function(bill) {
             res.redirect('/home');
+        }).catch(
+            function (err) {
+                req.flash('message', 'Error');
+                res.redirect('/billing/' + req.params.id);
+            }
+        );
+    });
+});
+
+router.get('/:id/transfer', isAuthenticated, function(req, res) {
+    models.Bill.findAll({
+        where: {
+            userId: req.user.id,
+            id: {
+                $ne: req.params.id
+            },
+            balance: {
+                $gt: 0
+            }
+        },
+        include: [
+            { model: models.Currency }
+        ]
+    }).then(function(bills) {
+        res.render('billing/transfer', {
+            user: req.user,
+            billId: req.params.id,
+            bills: bills,
+            message: req.flash('message')
         });
     });
 });
+
+router.post('/:id/transfer', isAuthenticated, function(req, res) {
+    if (isNaN(req.body.transfer) || req.body.transfer <= 0) {
+        req.flash('message', 'Invalid value');
+        return res.redirect('/billing/' + req.params.id + '/transfer');
+    }
+    var transfer = Number(req.body.transfer);
+    return models.Bill.findOne({
+        where: {
+            userId: req.user.id,
+            id: req.body.bill_id
+        }
+    }).then(
+        function(billFrom) {
+            models.Bill.findOne({
+                where: {
+                    userId: req.user.id,
+                    id: req.params.id
+                }
+            }).then(
+                function(billTo) {
+                    if (billFrom.currencyId == billTo.currencyId) {
+                        models.Bill.update(
+                            { balance: billTo.balance + transfer },
+                            { where: {id: billTo.id, userId: req.user.id} }
+                        ).then(
+                            function() {
+                                models.Bill.update(
+                                    { balance: billFrom.balance - transfer },
+                                    { where: {id: billFrom.id, userId: req.user.id} }
+                                ).then(
+                                    function() {
+                                        res.redirect('/billing/' + billTo.id);
+                                    }
+                                );
+                            }
+                        );
+                    } else {
+                        models.CurrencyConverter.findOne({
+                            where: {
+                                fromCurrencyId: billFrom.currencyId,
+                                toCurrencyId: billTo.currencyId
+                            }
+                        }).then(
+                            function(convertItem) {
+                                var newValue = billTo.balance + convertItem.rate * transfer;
+                                models.Bill.update(
+                                    { balance: newValue }, {where: {id: billTo.id, userId: req.user.id}}
+                                ).then(
+                                    function() {
+                                        models.Bill.update(
+                                            { balance: billFrom.balance - transfer }, {where: {id: billFrom.id, userId: req.user.id}}
+                                        ).then(
+                                            function() {
+                                                res.redirect('/billing/' + billTo.id);
+                                            }
+                                        );
+                                    }
+                                );
+                            }
+                        );
+                    }
+                }
+            );
+        }
+    );
+});
+
 module.exports = router;
