d7adfa8b0f8fcd4ebb05f75334e992495be010a1
ilya_r <magoz@mail.ru>
2015-12-04 03:55:11 +0500

    поправки в миграции индексов

--- migrations/20151129173235-add-index.js
+++ migrations/20151129173235-add-index.js
@@ -3,18 +3,25 @@
 module.exports = {
   up: function (queryInterface, Sequelize) {
     queryInterface.addIndex(
-        'Bills', ['user_id']
+        'Bills', ['userId']
     );
     queryInterface.addIndex(
-        'Users', ['login'], {
-          indexName: 'login',
+      'CurrencyConverters', ['fromCurrencyId', 'toCurrencyId'], {
+          indexName: 'from_to_Currency_id',
+          indicesType: 'UNIQUE'
+      }
+    );
+    queryInterface.addIndex(
+        'Users', ['username'], {
+          indexName: 'username',
           indicesType: 'UNIQUE'
         }
     );
   },
 
   down: function (queryInterface, Sequelize) {
-    queryInterface.removeIndex('Bills', 'user_id');
-    queryInterface.removeIndex('Users', 'login');
+    queryInterface.removeIndex('Bills', 'userId');
+      queryInterface.removeIndex('CurrencyConverters', 'from_to_Currency_id');
+      queryInterface.removeIndex('Users', 'username');
   }
 };
